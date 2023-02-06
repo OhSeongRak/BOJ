5f492a371ba5b85c715b12487ad4eb3882365413
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-06 17:40:56 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 140 ms, Memory: 20,440 KB -BaekjoonHub

---
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			String N = sc.next();
+			char curNum = '0';
+			int count = 0;
+			
+			for (int i = 0; i < N.length(); i++) {
+				if (curNum == N.charAt(i))
+					continue;
+				curNum = N.charAt(i);
+				count++;
+			}
+
+			System.out.printf("#%d %d%n", test_case, count);
+		} // end of test_case
+	} // end of main
+}
