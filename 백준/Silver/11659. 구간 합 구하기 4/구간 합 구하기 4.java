e80d9bec3a66e3dc0c80ae5f14520a77e04ad5cf
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-08 12:42:03 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2136 ms, Memory: 62520 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // br.readLine();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N + 1];
+		int[] cumSum = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 1; i <= N; i++) {
+			cumSum[i] = cumSum[i - 1] + arr[i];
+		}
+
+//		System.out.println(Arrays.toString(arr));
+//		System.out.println(Arrays.toString(cumSum));
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			System.out.println(cumSum[end] - cumSum[start - 1]);
+		}
+
+	} // end of main
+}
