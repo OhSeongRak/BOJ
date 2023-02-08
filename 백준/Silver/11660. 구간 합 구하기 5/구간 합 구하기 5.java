c5fb6f73433bc70c56f94b7b05aa2604478e6eff
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-08 16:11:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 948 ms, Memory: 111300 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,49 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // br.readLine();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
 
-		int[][] prefixSum = new int[N + 1][N + 1];
+    public static void main(String[] args) throws IOException {
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        //int numberCount = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				prefixSum[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int arrSize = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[arrSize][arrSize];
+        int[][] arrSum = new int[arrSize+1][arrSize+1];
 
-		// 가로 누적합
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				prefixSum[i][j] += prefixSum[i][j - 1];
-			}
-		}
+        int lines = Integer.parseInt(st.nextToken());
 
-		// 세로 누적합
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				prefixSum[j][i] += prefixSum[j - 1][i];
-			}
-		}
+        int low = 0;
+        while(arrSize-- > 0){
+            st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            int col = 0;
+            while(st.hasMoreTokens())
+                arr[low][col++] = Integer.parseInt(st.nextToken());
+            low++;
+        }
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
+        arrSum[1][1] = arr[0][0];
 
-			System.out.println(
-					prefixSum[x2][y2] - prefixSum[x2][y1 - 1] - prefixSum[x1 - 1][y2] + prefixSum[x1 - 1][y1 - 1]);
-		}
+        // arrSum 컨트롤
+        for(int i = 2 ; i<arr.length+1; i++)
+            arrSum[1][i] = arr[0][i-1] + arrSum[1][i-1];
 
-	} // end of main
+        for(int i = 2 ; i<arr.length+1; i++)
+            arrSum[i][1] = arr[i-1][0] + arrSum[i-1][1];
+
+        for(int i = 2 ; i<arr.length+1; i++) { // low
+            for(int j = 1+1; j<arr.length+1; j++){
+                arrSum[i][j] = arr[i-1][j-1] + arrSum[i][j-1] + arrSum[i-1][j] - arrSum[i-1][j-1];
+            }
+        }
+
+        // 질의
+        while(lines --> 0){
+            st = new StringTokenizer(br.readLine());
+            int sLow = Integer.parseInt(st.nextToken());
+            int sCol = Integer.parseInt(st.nextToken());
+            int eLow = Integer.parseInt(st.nextToken());
+            int eCol = Integer.parseInt(st.nextToken());
+
+            int answer = arrSum[eLow][eCol] - arrSum[eLow][sCol-1] - arrSum[sLow-1][eCol] + arrSum[sLow-1][sCol-1];
+            sb.append(answer).append("\n");
+
+        }
+
+        System.out.println(sb);
+
+
+
+    }
 }
+
+
