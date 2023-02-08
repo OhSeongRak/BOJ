a794525b14e0e2e8e6bfe102a6d486a2bf894734
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-08 14:44:27 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // br.readLine();
+
+		nineDwarfs = new int[9];
+		sevenDwarfs = new int[7];
+		for (int i = 0; i < 9; i++) {
+			nineDwarfs[i] = Integer.parseInt(br.readLine());
+		}
+
+		int cnt = 0;
+		int sum = 0;
+		int start = 0;
+		findDwarf(sum, cnt, start);
+
+	} // end of main
+
+	static int[] nineDwarfs;
+	static int[] sevenDwarfs;
+
+	public static void findDwarf(int sum, int cnt, int start) {
+		if (cnt == 7) {
+			if (sum == 100) {
+				for(int k : sevenDwarfs)
+					System.out.println(k);
+			}
+			return;
+		}
+
+		for (int i = start; i < 9; i++) {
+			sevenDwarfs[cnt] = nineDwarfs[i];
+			findDwarf(sum + nineDwarfs[i], cnt + 1, i + 1);
+		}
+
+	} // end of fdMethod
+} // end of class
