e67f0eaa2facc7b80076deb8047ee3e2c74b47c5
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-08 09:27:21 +0900

    [Silver III] Title: 숫자 야구, Time: 196 ms, Memory: 19972 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,9 +7,10 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	static Set<String> numSet = new HashSet<>(); // 초기값 : 111~999
+	static Set<String> numSet = new HashSet<>(); // 모은 정보들을 통해 정답이 될 수 있는 숫자들의 집합
 
 	// 중복 체크 함수
+	// 중복 숫자가 있거나, 0이 있으면 false
 	static boolean canNumber(String s) {
 		if (s.charAt(0) == s.charAt(1) || s.charAt(0) == s.charAt(2) || s.charAt(1) == s.charAt(2))
 			return false;
@@ -18,14 +20,15 @@ public class Main {
 		return true;
 	}
 
-	// 1. 가능한 숫자를 찾아서 tmpSet에 추가
-	// 2. numSet을 tmpSet과의 교집합만 남김.
+	// numSet : 여태껏의 strike, ball 정보로 가능한 숫자들의 집합 -> 무조건 numSet의 숫자들 중 하나가 정답
+	// numSet에 포함되어있고, 매개변수로 들어오는 strike와 ball로 가능한 숫자를 tmpSet에 넣음
+	// numSet = tmpSet을 해줌.
 	static void findPossibleNumber(String numStr, int strike, int ball) {
 		Set<String> tmpSet = new HashSet<>();
 
 		for (int i = 111; i < 1000; i++) {
-			String tmp = String.valueOf(i);
-			if (!numSet.contains(tmp)) // numSet에 있는 숫자 => 가능한 경우의 수
+			String tmp = String.valueOf(i); // 111 ~ 999를 문자열로
+			if (!numSet.contains(tmp)) // numSet에 없다면 후보군조차 될 수 없음
 				continue;
 			int sCount = 0; // tmp와 numStr 간의 strike 개수
 			int bCount = 0; // tmp와 numStr 간의 ball 개수
@@ -40,7 +43,7 @@ public class Main {
 				tmpSet.add(tmp);
 		}
 
-		numSet = tmpSet; // numSet과 tmpSet의 교집합만 numSet에 남김
+		numSet = tmpSet;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -55,15 +58,12 @@ public class Main {
 			numSet.add(tmp); // 사용 가능한 숫자면 numSet에 넣어줌
 		}
 
-//		for (String s: numSet)
-//			System.out.println(s);
-//
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			String numStr = st.nextToken();
 			int strike = Integer.parseInt(st.nextToken());
 			int ball = Integer.parseInt(st.nextToken());
-			findPossibleNumber(numStr, strike, ball); // 주어진 입력값을 통해 가능한 경우의 모든 숫자를 numSet에 남김
+			findPossibleNumber(numStr, strike, ball); // 가능한 숫자를 찾아 numSet을 조절해주는 함수
 		}
 //
 		System.out.println(numSet.size());
