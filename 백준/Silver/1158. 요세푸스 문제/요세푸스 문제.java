3935a48d06846e603a7b13ca27e426addc59bada
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-13 16:56:46 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 620 ms, Memory: 295052 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,59 +1,41 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // br.readLine();
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine()); // Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
 
-		CircularLinkedList cll = new CircularLinkedList();
-		for (int i = 0; i < N; i++) {
-			cll.add(i + 1);
-		}
-		cll.tail.next = cll.head;
 
-		Node pointer = cll.tail;
-		System.out.print('<');
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = 0; j < K - 1; j++) {
-				pointer = pointer.next;
-			}
-			System.out.print(pointer.next.data + ", ");
-			pointer.next = pointer.next.next;
+    public static void main(String[] args) throws IOException {
+        //int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		}
-		System.out.println(pointer.data + ">");
-	} // end of main
+        int number = Integer.parseInt(st.nextToken());
+        int turn = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 1; i<= number; i++){
+            q.add(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while(!q.isEmpty()){
+            for(int i= 0; i<turn; i++){
+                if(i == turn -1){
+                    sb.append(q.poll()).append(", ");
+                    break;
+                }
+                q.offer(q.poll());
+            }
+        }
+        sb.delete(sb.length()-2,sb.length());
+        sb.append(">");
+        System.out.println(sb);
+
+
+
+    }
 }
 
-class Node {
-	int data;
-	Node next;
-
-	public Node(int data, Node next) {
-		this.data = data;
-		this.next = next;
-	}
-}
-
-class CircularLinkedList {
-	public Node head = null;
-	public Node tail = null;
-
-	public void add(int data) {
-		Node newNode = new Node(data, null);
-		if (head == null) {
-			this.head = newNode;
-			this.tail = newNode;
-			return;
-		}
-		this.tail.next = newNode;
-		this.tail = newNode;
-	}
-}
