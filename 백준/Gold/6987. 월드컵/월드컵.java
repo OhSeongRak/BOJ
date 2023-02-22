7bafdd28a354704d77aa340a96e4eb1169e29b56
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-22 09:07:13 +0900

    [Gold V] Title: 월드컵, Time: 124 ms, Memory: 15052 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -1,77 +1,72 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
+/* TODO:
+ * 
+ *
+ * */
 public class Main {
-	static int[][] score;
-	static int[][] fight = { { 0, 1 }, { 0, 2 }, { 0, 3 }, { 0, 4 }, { 0, 5 }, { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 },
-			{ 2, 3 }, { 2, 4 }, { 2, 5 }, { 3, 4 }, { 3, 5 }, { 4, 5 } };
-	static int possible;
+	static boolean[] v = new boolean[6];
+	static int[][] score = new int[6][3];
+	static boolean isPos;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		for (int t = 0; t < 4; t++) {
-			score = new int[6][3];
-			possible = 0;
-			// input
-			st = new StringTokenizer(br.readLine(), " ");
+		tc : 
+		for (int k = 0; k < 4; k++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
 			for (int i = 0; i < 6; i++) {
+				int sum = 0;
 				for (int j = 0; j < 3; j++) {
 					score[i][j] = Integer.parseInt(st.nextToken());
+					sum += score[i][j]; 
 				}
+				if(sum != 5) {
+					sb.append(0).append(" ");
+					continue tc;
+				}
+					
 			}
-			// input end
 
-			find(0);
-			sb.append(possible).append(" ");
+			isPos = false;
+			dfs(0, 0, 0, 0, 0);
+			
+			sb.append(isPos? 1: 0).append(" ");
 		}
 		System.out.println(sb.toString());
+	}
 
-	} // end of main
-
-	private static void find(int cnt) {
-		if (possible == 1)
+	public static void dfs(int count, int t1, int t2, int point1, int point2) {
+		if(point1 < 0 || point2 < 0)
 			return;
-
-		if (cnt == 15) {
-			for (int i = 0; i < 6; i++)
-				for (int j = 0; j < 3; j++)
-					if (score[i][j] != 0)
-						return;
-
-			possible = 1;
+		
+		if (count == 15) {
+			isPos = true;
 			return;
 		}
 		
-		// i 승리
-		if (score[fight[cnt][0]][0] != 0 && score[fight[cnt][1]][2] != 0) {
-			score[fight[cnt][0]][0]--;
-			score[fight[cnt][1]][2]--;
-			find(cnt + 1);
-			score[fight[cnt][0]][0]++;
-			score[fight[cnt][1]][2]++;
+		// 팀 선택
+		t2++;
+		if(t2 == 6) {
+			t1++;
+			t2 = t1 + 1;
 		}
-
-		// 무승부
-		if (score[fight[cnt][0]][1] != 0 && score[fight[cnt][1]][1] != 0) {
-			score[fight[cnt][0]][1]--;
-			score[fight[cnt][1]][1]--;
-			find(cnt + 1);
-			score[fight[cnt][0]][1]++;
-			score[fight[cnt][1]][1]++;
-		}
-
-		// i 패배
-		if (score[fight[cnt][0]][2] != 0 && score[fight[cnt][1]][0] != 0) {
-			score[fight[cnt][0]][2]--;
-			score[fight[cnt][1]][0]--;
-			find(cnt + 1);
-			score[fight[cnt][0]][2]++;
-			score[fight[cnt][1]][0]++;
-		}
+				
+		// 승패
+		score[t1][0]--; score[t2][2]--;
+		dfs(count + 1, t1, t2, score[t1][0], score[t2][2]);
+		score[t1][0]++; score[t2][2]++;
+		// 무
+		score[t1][1]--; score[t2][1]--;
+		dfs(count + 1, t1, t2, score[t1][1], score[t2][1]);
+		score[t1][1]++; score[t2][1]++;
+		// 패승
+		score[t1][2]--; score[t2][0]--;
+		dfs(count + 1, t1, t2, score[t1][2], score[t2][0]);
+		score[t1][2]++; score[t2][0]++;
 	}
 }
+
