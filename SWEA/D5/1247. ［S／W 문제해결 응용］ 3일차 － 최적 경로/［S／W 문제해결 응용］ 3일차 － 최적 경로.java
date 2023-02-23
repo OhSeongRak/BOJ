40c5a9118a0662de5e12c15917bd54c7a66b713a
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-23 09:34:45 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 300 ms, Memory: 21,172 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -2,8 +2,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -11,10 +9,8 @@ public class Solution {
 	static StringTokenizer st;
 	static StringBuilder sb;
 	static int N;
-	static int[] input; // 사용자 입력받은 숫자 배열
 	static int[] home;
-	static int[] company;
-	static int[][] customer;
+	static int[][] customer; // 0번째는 회사
 	static int[] numbers; // 순열에서 뽑은 숫자 담을 배열
 	static boolean[] isSelected; // 사용한 숫자인지 체크할 플래그 배열
 	static int total;
@@ -23,7 +19,7 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for (int test_case = 1; test_case <= T; test_case++) {
 			input();
-			permutation(0);
+			permutation(1, 0);
 			sb = new StringBuilder();
 			sb.append("#").append(test_case).append(" ").append(total);
 			System.out.println(sb.toString());
@@ -33,46 +29,41 @@ public class Solution {
 	private static void input() throws IOException {
 		// input
 		N = Integer.parseInt(br.readLine());
-		input = new int[N];
 		home = new int[2];
-		company = new int[2];
-		customer = new int[N][2];
-		numbers = new int[N];
-		isSelected = new boolean[N];
+		customer = new int[N + 1][2];
+		numbers = new int[N + 1];
+		isSelected = new boolean[N + 1];
 		total = Integer.MAX_VALUE;
 		st = new StringTokenizer(br.readLine());
-		company[0] = Integer.parseInt(st.nextToken());
-		company[1] = Integer.parseInt(st.nextToken());
+		customer[0][0] = Integer.parseInt(st.nextToken());
+		customer[0][1] = Integer.parseInt(st.nextToken());
 		home[0] = Integer.parseInt(st.nextToken());
 		home[1] = Integer.parseInt(st.nextToken());
-		for (int i = 0; i < N; i++) {
-			input[i] = i;
+		for (int i = 1; i <= N; i++) {
 			customer[i][0] = Integer.parseInt(st.nextToken());
 			customer[i][1] = Integer.parseInt(st.nextToken());
 		}
 		// input end
 	}
 
-	public static void permutation(int index) {
-		if (index == N) {
-			int sum = 0;
-			sum += distance(company[0], company[1], customer[numbers[0]][0], customer[numbers[0]][1]);
-			for (int i = 0; i < N - 1; i++) {
-				sum += distance(customer[numbers[i]][0], customer[numbers[i]][1], customer[numbers[i + 1]][0],
-						customer[numbers[i + 1]][1]);
-			}
-			sum += distance(home[0], home[1], customer[numbers[N - 1]][0], customer[numbers[N - 1]][1]);
+	public static void permutation(int index, int sum) {
+		if (sum > total)
+			return;
+
+		if (index > N) {
+			sum += distance(customer[numbers[index - 1]][0], customer[numbers[index - 1]][1], home[0], home[1]);
 			total = Math.min(sum, total);
 			return;
 		}
 
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			if (isSelected[i])
 				continue;
 
-			numbers[index] = input[i];
+			numbers[index] = i;
 			isSelected[i] = true;
-			permutation(index + 1);
+			permutation(index + 1, sum + distance(customer[numbers[index - 1]][0], customer[numbers[index - 1]][1],
+					customer[numbers[index]][0], customer[numbers[index]][1]));
 			isSelected[i] = false;
 		}
 	}
