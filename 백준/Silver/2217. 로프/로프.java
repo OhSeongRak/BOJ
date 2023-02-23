fe0297c11bb0080011e0d90e904931b10b478592
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-23 10:40:46 +0900

    [Silver IV] Title: 로프, Time: 336 ms, Memory: 26404 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,15 +1,16 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
 		int[] cumSum = new int[N];
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		Arrays.sort(arr);
 		int answer = 0;
