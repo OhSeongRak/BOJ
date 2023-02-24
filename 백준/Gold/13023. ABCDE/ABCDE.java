0f05f2fe7e51acdef284a61501c923b65289a05c
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-02-24 12:09:21 +0900

    [Gold V] Title: ABCDE, Time: 236 ms, Memory: 22428 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,56 +1,66 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static boolean[] visited;
+	static boolean possible;
+	static List<Integer>[] friends;
+	static int N, M;
 
-	static List<List<Integer>> list = new ArrayList<>();
-	static boolean[] v;
-	static int maxDepth;
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int node = Integer.parseInt(st.nextToken());
-		int edge = Integer.parseInt(st.nextToken());
-
-		for (int i = 0; i < node; i++)
-			list.add(new ArrayList<>());
-
-		v = new boolean[node];
-
-		while (edge-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-
-			list.get(s).add(e);
-			list.get(e).add(s);
+	public static void main(String[] args) throws IOException {
+		input();
+		for (int i = 0; i < N; i++) {
+			if (possible) {
+				System.out.println(1);
+				return;
+			}
+			visited = new boolean[N];
+			dfs(1, i);
 		}
+		System.out.println(0);
+	} // end of main
 
-		for (int i = 0; i < node; i++) {
-			if (maxDepth == 4)
-				break;
-			v[i] = true;
-			dfs(i, 0);
-			v[i] = false;
-			Arrays.fill(v, false);
-		}
-		System.out.println(maxDepth == 4 ? 1 : 0);
-	}
+	private static void dfs(int distance, int key) {
+		if (possible)
+			return;
 
-	public static void dfs(int s, int depth) {
-		if (depth == 4) {
-			maxDepth = 4;
+		if (distance == 5) {
+			possible = true;
 			return;
 		}
 
-		for (int e : list.get(s)) {
-			if (!v[e]) {
-				v[e] = true;
-				dfs(e, depth + 1);
-				v[e] = false;
+		visited[key] = true;
+		for (int num : friends[key]) {
+			if (!visited[num]) {
+				dfs(distance + 1, num);
+				visited[num] = false;
 			}
 		}
-	}
-}
+	} // end of dfs
+
+	@SuppressWarnings("unchecked")
+	private static void input() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		friends = new ArrayList[N];
+		for (int i = 0; i < friends.length; i++) {
+			friends[i] = new ArrayList<Integer>();
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			friends[a].add(b);
+			friends[b].add(a);
+		}
+	} // end of input
+} // end of class
