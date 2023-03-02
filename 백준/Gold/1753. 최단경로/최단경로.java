a6618ba3242683607ae42e155a5834c265a62167
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2023-03-02 14:09:24 +0900

    [Gold IV] Title: 최단경로, Time: 756 ms, Memory: 108712 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,87 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static class Node implements Comparable<Node> {
-		int no;
-		int weight;
 
-		public Node(int no, int weight) {
-			this.no = no;
-			this.weight = weight;
-		}
+	static List<List<Node>> list = new ArrayList<>();
+	static int[] dist;
 
-		public int compareTo(Node o) {
-			return this.weight - o.weight;
-		}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
-	}
+		st = new StringTokenizer(br.readLine());
 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int V, E, start;
-	static List<Node>[] graph;
-	static int[] distance; // 출발 정점에서 자신까지 오는 최소 비용
-	static boolean[] visited;
-	static final int INF = Integer.MAX_VALUE;
+		int nodes = Integer.parseInt(st.nextToken());
+		int edges = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		input();
-		dijkstra();
-		for (int i = 1; i <= V; i++) {
-			if (distance[i] == INF)
-				System.out.println("INF");
-			else
-				System.out.println(distance[i]);
-		}
-	}
+		dist = new int[nodes + 1];
+		for (int i = 0; i <= nodes; i++) {
+			list.add(new ArrayList<>());
+			dist[i] = Integer.MAX_VALUE / 2;
+		} 
 
-	private static void dijkstra() {
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
-		Arrays.fill(distance, INF);
-		distance[start] = 0;
-		pq.add(new Node(start, 0));
+		int startNode = Integer.parseInt(br.readLine());
+		dist[startNode] = 0;
 
-		while (!pq.isEmpty()) {
-			Node curNode = pq.poll();
+		while (edges-- > 0) {
+			st = new StringTokenizer(br.readLine());
 
-			if (distance[curNode.no] < curNode.weight)
-				continue;
-
-			for (Node node : graph[curNode.no]) {
-				int total = distance[curNode.no] + node.weight;
-				if (total < distance[node.no]) {
-					distance[node.no] = total;
-					pq.add(new Node(node.no, total));
-				}
-			}
-		}
-	} // end of dijkstra
-
-	private static void input() throws IOException {
-		st = new StringTokenizer(br.readLine(), " ");
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		start = Integer.parseInt(br.readLine());
-		graph = new ArrayList[V + 1];
-		distance = new int[V + 1];
-		visited = new boolean[V + 1];
-
-		for (int i = 1; i <= V; i++)
-			graph[i] = new ArrayList<Node>();
-
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			graph[s].add(new Node(e, w));
+
+			list.get(s).add(new Node(e, w));
 		}
-	} // end of input
-} // end of class
+
+		dijk(startNode);
+
+		for (int i = 1; i < list.size(); i++) {
+			if (dist[i] == Integer.MAX_VALUE / 2)
+				sb.append("INF").append("\n");
+			else
+				sb.append(dist[i]).append("\n");
+		}
+
+		System.out.println(sb.toString());
+
+	}
+
+	public static void dijk(int startNode) {
+		Queue<Node> q = new PriorityQueue<>((o1, o2 )->o1.w - o2.w);
+		q.offer(new Node(startNode, 0));
+
+		while (!q.isEmpty()) {
+			Node mid = q.poll();
+
+			if (mid.w > dist[mid.e])
+				continue;
+
+			for (Node dest : list.get(mid.e)) {
+				if (dist[dest.e] > dist[mid.e] + dest.w) {
+					dist[dest.e] = dist[mid.e] + dest.w;
+					q.offer(new Node(dest.e, dist[dest.e]));
+				}
+			}
+		}
+	}
+
+	static class Node {
+		int e, w;
+
+		public Node(int e, int w) {
+			this.e = e;
+			this.w = w;
+		}
+	}
+}
